89bc383a4de6497253b7439bb8667226093b7d44
Garrett Johnson <itwinx@yahoo.com>
2017-04-19 20:59:21 +0000

    Added a function model with name and Javascript fields.

--- grapher/models.py
+++ grapher/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Function(models.Model):
+    name = models.CharField(max_length=200)
+    js = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
