8d4dee705dbb6a274004b26fb761b15629708151
Garrett Johnson <itwinx@yahoo.com>
2017-04-19 06:38:21 +0000

    Correctly graphs integer values in a set domain.

--- grapher/static/grapher/graph2d.js
+++ grapher/static/grapher/graph2d.js
@@ -1,19 +1,18 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-function Point2D(x,y) {
-    this.x = x;
-    this.y = y;
-}
-
 function drawGraph(points) {
     context.beginPath();
-    context.moveTo(0,points[0]*10);
+    context.moveTo(0,canvas.height/2-points[0]*canvas.height/10);
     for (var i = 1; i < points.length; i++) {
-        context.lineTo(i*10,points[i]*10);
+        context.lineTo(i*canvas.width/10,canvas.height/2-points[i]*canvas.height/10);
     }
     context.stroke();
 }
 
-var points = [16,9,4,1,0,1,4,9,16];
+var points = [25,16,9,4,1,0,1,4,9,16,25];
+/*var points = [];
+for (var i = 0; i < 1001; i++) {
+    points[i] = (i/100 - 5);
+}*/
 drawGraph(points);
